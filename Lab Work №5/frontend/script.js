1c103ba7f5002cd7b33a39a7d69f9c1df666b75c
MariaSkur <113085488+MariaSkur@users.noreply.github.com>
2026-03-11 23:15:06 +0500

    Update script.js

--- Lab Work №5/frontend/script.js
+++ Lab Work №5/frontend/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api/projects';const API_URL = 'http://localhost:5000/api/projects';
+const API_URL = window.location.protocol + '//' + window.location.hostname + ':5000/api/projects';
 
 async function loadProjects() {
     const response = await fetch(API_URL);
