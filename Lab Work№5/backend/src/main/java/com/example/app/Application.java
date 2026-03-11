725193e1fa2f5c9444f2c9937e96dc464bf166ba
MariaSkur <113085488+MariaSkur@users.noreply.github.com>
2026-03-11 23:57:56 +0500

    Create Application.java

---
+++ Lab Work№5/backend/src/main/java/com/example/app/Application.java
@@ -0,0 +1,20 @@
+package com.example.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/venues")
+    public String venues() {
+        return "List of venues from backend";
+    }
+
+}
