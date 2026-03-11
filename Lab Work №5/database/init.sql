cbdd316dc9a8210fea6dc1a4d70b87fb115a4533
MariaSkur <113085488+MariaSkur@users.noreply.github.com>
2026-03-11 23:51:02 +0500

    Delete Lab Work №5 directory

--- Lab Work №5/database/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS projects (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,         -- название проекта
-    date DATE NOT NULL,                  -- дата мероприятия
-    location VARCHAR(255) NOT NULL,       -- площадка / место проведения
-    description TEXT                       -- описание, комментарии
-);
