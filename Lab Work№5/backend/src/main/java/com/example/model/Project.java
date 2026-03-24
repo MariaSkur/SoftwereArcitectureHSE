0d04bf867e6ae78ea529254a5dd4d5c1005927cf
MariaSkur <113085488+MariaSkur@users.noreply.github.com>
2026-03-24 21:35:47 +0500

    Create Project.java

---
+++ Lab Work№5/backend/src/main/java/com/example/model/Project.java
@@ -0,0 +1,42 @@
+package com.example.model;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "projects")
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private LocalDate date;
+    private String location;
+    private String description;
+
+    public Project() {}
+
+    public Project(String name, LocalDate date, String location, String description) {
+        this.name = name;
+        this.date = date;
+        this.location = location;
+        this.description = description;
+    }
+
+    // геттеры и сеттеры
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public LocalDate getDate() { return date; }
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public String getLocation() { return location; }
+    public void setLocation(String location) { this.location = location; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
